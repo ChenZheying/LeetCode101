35cde9bb177963509eef91429dc9e67712f41a27
ChenZheying <rjfuture@live.com>
2022-03-20 09:35:39 +0800

    0320

--- 05Search/257_binaryTreePaths.cpp
+++ 05Search/257_binaryTreePaths.cpp
@@ -3,10 +3,30 @@
 //
 #include <string>
 #include <vector>
-
+#include "TreeNode.h"
+using namespace std;
 class Solution {
 public:
+    // 很明显是深度优先遍历
+    void dfs(TreeNode* root, string& path, vector<string>& ans) {
+        if(root == nullptr) {return;}
+        if(root->left == nullptr && root->right == nullptr)
+        {
+            ans.push_back(path);
+            return;
+        }
+        path+=("->"+ to_string(root->val));
+        dfs(root->left, path, ans);
+        dfs(root->right, path,ans);
+    }
     vector<string> binaryTreePaths(TreeNode* root) {
-
+        vector<string> ans;
+        if(root == nullptr) {
+            return ans;
+        }
+        string path = to_string(root->val);
+        dfs(root->left,path,ans);
+        dfs(root->right,path,ans);
+        return ans;
     }
 };
